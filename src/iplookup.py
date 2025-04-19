8225c1ef1e916cec4861884000146b72b6a20aca
Tim Matheis <tim.matheis@outlook.com>
2025-04-19 14:53:29 +0200

    Implemented IPv6 Lookup

--- src/iplookup.py
+++ src/iplookup.py
@@ -18,20 +18,18 @@ from sys import argv
 from string import hexdigits
 
 
-
-def request_ipinfo_api(ip_version: int, requested_ip) -> int:
-    if ip_version == 4:
-        ipinfo_api_requested_url = f"https://ipinfo.io/{requested_ip}/json"
+def request_ipinfo_api(requested_ip: str) -> int:
+    if requested_ip == "self":
+        ipinfo_api_requested_url = f"https://ipinfo.io/json"
     else:
-        ipinfo_api_requested_url = f"https://v6.ipinfo.io/{requested_ip}/json"
-        print(ipinfo_api_requested_url)
+        ipinfo_api_requested_url = f"https://ipinfo.io/{requested_ip}/json"
     ipinfo_api_call = requests.get(ipinfo_api_requested_url)
     ipinfo_api_response_statuscode = ipinfo_api_call.status_code
     ipinfo_api_response_json = json.loads(ipinfo_api_call.text)
     ipinfo_api_response_coord_latitude, ipinfo_api_response_coord_longitude = f"{ipinfo_api_response_json["loc"]}".split(',')
     google_maps_link = f"https://www.google.com/maps/search/?api=1&query={ipinfo_api_response_coord_latitude},{ipinfo_api_response_coord_longitude}"
     print("\n")
-    print(f"The IP '{requested_ip}' is located in {ipinfo_api_response_json["postal"]} {ipinfo_api_response_json["city"]} in {ipinfo_api_response_json["region"]}, {ipinfo_api_response_json["country"]}")
+    print(f"The IP '{requested_ip}' is located in '{ipinfo_api_response_json["postal"]} {ipinfo_api_response_json["city"]}' in '{ipinfo_api_response_json["region"]}, {ipinfo_api_response_json["country"]}'")
     print(f"The coordinates are: {ipinfo_api_response_json["loc"]}")
     print(f"Google Maps URI\n{google_maps_link}")
     print("\n")
@@ -58,25 +56,59 @@ def is_valid_ipv4(ip: str) -> bool:
 
     return True
 
-def is_ipv4(ip: str) -> bool:
-    if ip.count(".") > 0:
-        return True
-    return False
+def expand_ipv6(ip: str) -> str:
+    hextets = ip.split(":")
+    missing_hextets = 8 - len(hextets) + 1
+
+    find_index = ip.find("::") + 1
+
+    # gets the index in hextets where the '::' was found
+    counter = 0
+    for i, hextet in enumerate(hextets):
+        counter += len(hextet) + 1
+        if counter == find_index:
+            double_column_index = i + 1
+    hextets.pop(double_column_index)
+
+    # expand IPv6 Address
+    for i in range(missing_hextets):
+        hextets.insert(double_column_index, "0000")
+    if hextets[-1] == "":
+        hextets.pop()
+        hextets.append("0000")
+
+    # expand every hextet in hextets
+    for i, hextet in enumerate(hextets):
+        missing_characters = 4 - len(hextet)
+        if missing_characters > 0:
+            new_str = f"{"0" * missing_characters}{hextet}"
+            hextets.pop(i)
+            hextets.insert(i, new_str)
+    ip: str = ""
+    for i, hextet in enumerate(hextets):
+        ip += hextet
+        if not i == (len(hextets) - 1):
+            ip += ":"
+    return ip
 
 def is_valid_ipv6(ip: str) -> bool:
+    if not isinstance(ip, str):
+        print(f"Type of 'ip' is expected to be a str, not {type(ip)}")
     if ip == "":
-        print("Didn't pass an IP Address.")
+        print("Didn't pass an IPv6 Address.")
         return False
     if not ip.count(":") == 7:
-        print("Please enter an valid IPv6 (e.g. 2001:0db8:0000:0044:5555:6666:7777:8888)")
-        return False
+        print("Detected shortened IPv6 Address.")
+        if ip.count("::") != 1:
+            print("Shortened IPv6 Addresses only allow the occurrence of '::' once.")
+            return False
+        expanded_ip = expand_ipv6(ip).split(":")
+    else:
+        expanded_ip = ip.split(":")
 
-    hextets = ip.split(":")
-    for hextet_nr, hextet in enumerate(hextets):
+    for hextet_nr, hextet in enumerate(expanded_ip):
         for character_nr, character in enumerate(hextet):
-            if character in hexdigits:
-                continue
-            else:
+            if character not in hexdigits:
                 print(f"No hexdigit ({character}) in hextet {hextet_nr + 1} at character {character_nr + 1}")
                 return False
     return True
@@ -85,13 +117,9 @@ def is_valid_ipv6(ip: str) -> bool:
 def get_args() -> argparse:
     parser = argparse.ArgumentParser(description="IPv4 Lookup shortcuts\nCommand example: python iplookup.py --ip 123.123.123.123")
     parser.add_argument("-o", "--own-ip", action="store_true", dest="own_ip", help="This will get your current IP look it up and show it.")
-    parser.add_argument("-ip", "--ip", action="store", dest="ip", type=str, help="Enter valid IP to look up.")
+    parser.add_argument("-ipv4", "--ipv4", action="store", dest="ipv4", type=str, help="Enter valid IPv4-Address to look up.")
+    parser.add_argument("-ipv6", "--ipv6", action="store", dest="ipv6", type=str, help="Enter valid IPv6-Address to look up.")
     return parser.parse_args()
-
-def get_own_ipv4() -> str: # TODO: checkip.amazonaws.com may return mulitple IP Addresses. Currently only handles one IP returned.
-    api_uri = "https://checkip.amazonaws.com/"
-    api_call = requests.get(api_uri)
-    return api_call.text[:-1] # cuts new-line at the end of the response
 
 def main():
     args_passed: bool = False
@@ -100,32 +128,45 @@ def main():
         try:
             args = get_args()
             args_passed: bool = True
-        except SystemExit: # This exception is raised when -h or --help is called and help is printed
+        except SystemExit:  # This exception is raised when -h or --help is called and help is printed
             quit("Exiting. User entered '-h/--help'")
         except Exception as e:
-            print(f"E: Error parsing arguments: {e}\nUsing interactive mode instead")
+            print(f"Error parsing arguments: {e}\nUsing interactive mode instead.")
     else:
         print("No arguments provided. Using interactive mode.")
 
-
     if args_passed:
         if args.own_ip:
-            requested_ip = get_own_ipv4()
+            request_ipinfo_api(4, "self")
+            quit()
         else:
-            requested_ip = args.ip
+            if args.ipv4 is None and args.ipv6 is None:
+                quit("No IPv4 or IPv6 Address specified. Pass one IP Address for one IP Version.")
+            if args.ipv4 is not None and args.ipv6 is not None:
+                quit("IPv4 and IPv6 Address specified. Only use one IP Version at a time.")
+
+            if args.ipv4 is not None:
+                if is_valid_ipv4(args.ipv4):
+                    api_statuscode: int = request_ipinfo_api(args.ipv4)
+                    if not api_statuscode == 200:
+                        print(f"API call failed\nYour API call returned {api_statuscode} status code. Check your WAN-Connection.")
+            else:
+                if is_valid_ipv6(args.ipv6):
+                    api_statuscode: int = request_ipinfo_api(expand_ipv6(args.ipv6))
+                    if not api_statuscode == 200:
+                        print(f"API call failed\nYour API call returned {api_statuscode} status code. Check your WAN-Connection.")
     else:
         requested_ip: str = input("Enter IP: ")
-
-    if is_ipv4(requested_ip):
-        if is_valid_ipv4(requested_ip):
-            api_statuscode: int = request_ipinfo_api(4, requested_ip)
-            if not api_statuscode == 200:
-                print(f"API call failed\nYour API call returned {api_statuscode} status code.")
-    else:
-        if is_valid_ipv6(requested_ip):
-            api_statuscode: int = request_ipinfo_api(6, requested_ip)
-            if not api_statuscode == 200:
-                print(f"API call failed\nYour API call returned {api_statuscode} status code.")
+        if requested_ip.count(".") > 0:
+            if is_valid_ipv4(requested_ip):
+                api_statuscode: int = request_ipinfo_api(requested_ip)
+                if not api_statuscode == 200:
+                    print(f"API call failed\nYour API call returned {api_statuscode} status code. Check your WAN-Connection.")
+        else:
+            if is_valid_ipv6(requested_ip):
+                api_statuscode: int = request_ipinfo_api(expand_ipv6(requested_ip))
+                if not api_statuscode == 200:
+                    print(f"API call failed\nYour API call returned {api_statuscode} status code. Check your WAN-Connection.")
 
 
 if __name__ == "__main__":
